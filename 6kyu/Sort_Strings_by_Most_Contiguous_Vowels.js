9694653edf33e26a87285ff84799b88b47642a4d
Dmitry Gulevsky <d.gulevsky@mail.ru>
2020-10-16 09:15:31 +0300

    6kyu

---
+++ 6kyu/Sort_Strings_by_Most_Contiguous_Vowels.js
@@ -0,0 +1,21 @@
+//Sort Strings by Most Contiguous Vowels
+
+function sortStringsByVowels(arr) {
+  return arr.sort((a, b) => checkLengthVowels(b) - checkLengthVowels(a));
+}
+function checkLengthVowels(str) {
+  if (str.length == 0) return 0;
+  const vow = "aeiouAEIOU";
+  let s = 0;
+  let max = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (vow.indexOf(str[i]) > -1) {
+      s++;
+    }
+    if (vow.indexOf(str[i]) == -1) {
+      s = 0;
+    }
+    if (s > max) max = s;
+  }
+  return max;
+}

636d774b47c4ccb648f2a0b5930d96c13aed91de
Dmitry Gulevsky <d.gulevsky@mail.ru>
2020-10-17 10:44:28 +0300

    6kyu

---
+++ 6kyu/extract_file_name.js
@@ -0,0 +1,9 @@
+//extract file name
+
+class FileNameExtractor {
+  static extractFileName(dirtyFileName) {
+    let start = dirtyFileName.indexOf("_"),
+      end = dirtyFileName.lastIndexOf(".");
+    return dirtyFileName.slice(start + 1, end);
+  }
+}

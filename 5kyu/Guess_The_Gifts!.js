00eca00318032109cf8bedd45891b7b8839428ab
Dmitry Gulevsky <d.gulevsky@mail.ru>
2020-10-18 16:26:22 +0300

    5kyu

---
+++ 5kyu/Guess_The_Gifts!.js
@@ -0,0 +1,22 @@
+//Guess The Gifts!
+
+function guessGifts(wishlist, presents) {
+  const res = [];
+  for (let i = 0; i < presents.length; i++) {
+    const present = [
+      presents[i].size,
+      presents[i].clatters,
+      presents[i].weight,
+    ];
+    for (let j = 0; j < wishlist.length; j++) {
+      const wish = [wishlist[j].size, wishlist[j].clatters, wishlist[j].weight];
+      if (
+        wish.filter((el, i) => el == present[i]).length == 3 &&
+        res.indexOf(wishlist[j].name) === -1
+      ) {
+        res.push(wishlist[j].name);
+      }
+    }
+  }
+  return res;
+}

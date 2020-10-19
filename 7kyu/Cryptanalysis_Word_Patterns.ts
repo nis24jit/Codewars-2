c2b8bfc3001c78c321450c8e2adecd915aa926a3
Dmitry Gulevsky <d.gulevsky@mail.ru>
2020-10-19 12:37:00 +0300

    7kyu

---
+++ 7kyu/Cryptanalysis_Word_Patterns.ts
@@ -0,0 +1,18 @@
+//Cryptanalysis Word Patterns
+
+export function wordPattern(word: string): string {
+  let s: string = "";
+  let res: string = "";
+  word = word.toLowerCase();
+  for (let i = 0; i < word.length; i++) {
+    if (s.indexOf(word[i]) === -1) {
+      s += word[i];
+    }
+    if (i < word.length - 1) {
+      res += s.indexOf(word[i]) + ".";
+    } else {
+      res += s.indexOf(word[i]);
+    }
+  }
+  return res;
+}

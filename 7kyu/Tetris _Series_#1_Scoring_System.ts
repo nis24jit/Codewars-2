5df952154e6b6049d75b05e74aeda5f99fed3528
Dmitry Gulevsky <d.gulevsky@mail.ru>
2020-10-19 15:40:08 +0300

    7kyu

---
+++ 7kyu/Tetris _Series_#1_Scoring_System.ts
@@ -0,0 +1,16 @@
+// /Tetris Series #1 — Scoring System
+
+export function getScore(arr: number[]) {
+  let num: number[] = [0, 40, 100, 300, 1200];
+  let c = 1;
+  let p = 0;
+  let m = 10;
+  return arr.reduce((a: number, b: number, i) => {
+    if (p >= m) {
+      c++;
+      m += 10;
+    }
+    p += b;
+    return a + num[b] * c;
+  }, 0);
+}

c8adfdff801432342142d013d5431c0381ff3a51
Dmitry Gulevsky <d.gulevsky@mail.ru>
2020-10-20 09:53:35 +0300

    6kyu

---
+++ 6kyu/Casino_chips.js
@@ -0,0 +1,14 @@
+//Casino chips
+
+function solve(arr) {
+  let count = 0;
+  arr = arr.sort((a, b) => b - a);
+  while (arr[1]) {
+    if (arr[2]) arr[2]--;
+    else arr[1]--;
+    count++;
+    arr[0]--;
+    arr[1] > arr[0] ? ([arr[1], arr[0]] = [arr[0], arr[1]]) : arr;
+  }
+  return count;
+}

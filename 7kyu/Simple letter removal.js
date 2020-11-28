950cb55925f0d5de2352520c2a617698b71c1bf5
gulevskydev <d.gulevsky@mail.ru>
2020-11-28 14:33:20 +0300

    1kyu

---
+++ 7kyu/Simple letter removal.js
@@ -0,0 +1,15 @@
+//Simple letter removal
+
+function solve(s, k) {
+    const sk = s.split('').sort().slice(0, k)
+    const obj = {};
+    sk.forEach(v => obj[v] = obj[v] ? obj[v] + 1 : 1)
+    s = s.split('').map(e => {
+        if (obj[e] > 0) {
+            obj[e]--
+            return ''
+        }
+        return e
+    })
+    return s.join('')
+}

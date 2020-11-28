6f93da7984babab57ff580a5be77c72f6e645826
gulevskydev <d.gulevsky@mail.ru>
2020-11-28 14:00:19 +0300

    7kyu

---
+++ 7kyu/f.js
@@ -0,0 +1,11 @@
+function doubles(s){
+    s = s.split('')
+    let res = ''
+      for(let i = 0; i < s.length; i++){
+        if(s[i] == s[i+1]) {
+          s.splice(i,2)
+          i = -1
+        }
+      } 
+    return s.join('')
+  }

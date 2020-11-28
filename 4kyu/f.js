0602e26d0edf95573e2d766e528d334f242e86d2
gulevskydev <d.gulevsky@mail.ru>
2020-11-28 13:37:14 +0300

    4kyu

---
+++ 4kyu/f.js
@@ -0,0 +1,12 @@
+const mem = (n, fn) => {
+    const memo = {}
+    return (...arr) => {
+        const i = n(...arr)
+        return memo[i] || (memo[i] = fn(...arr))
+    }
+}
+const count = mem(
+    (n, a) => `${n}~${a}`,
+    (n, a) => a > n ? 0 : a == n ? 1 : count(n - a, a) + count(n, a + 1)
+)
+const sum = (n) => count(n, 1)

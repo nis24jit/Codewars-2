b5f85675e6f31dea90a9e30855c41f13fcbe791d
gulevskydev <d.gulevsky@mail.ru>
2020-11-30 22:09:45 +0300

    7kyu

---
+++ 7kyu/The Poet And The Pendulum.js
@@ -0,0 +1,15 @@
+//The Poet And The Pendulum
+
+function pendulum(values) {
+    let r = []
+    let l = []
+    values = values.sort((a, b) => a - b)
+    for (let i = 0; i < values.length; i++) {
+        if (i % 2 == 0) {
+            r.push(values[i])
+        } else {
+            l.push(values[i])
+        }
+    }
+    return values.length % 2 == 0 ? [...r.reverse(), ...l] : [...r.reverse(), ...l]
+}

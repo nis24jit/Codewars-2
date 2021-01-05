5d72dae4739ab72aa1d9a801b7a5aa8bec41c3b7
gulevskydev <d.gulevsky@mail.ru>
2021-01-05 10:25:45 +0300

    5kyu

---
+++ 5kyu/Simple Events.js
@@ -0,0 +1,13 @@
+function Event() {
+    this.observers = []
+    this.subscribe = function  (fn) {
+      this.observers.push(fn)
+    }
+  
+    this.unsubscribe = function (fn) {
+      this.observers = this.observers.filter(subscriber => subscriber !== fn)
+    }
+     this.emit =  function (...data) {
+      this.observers.forEach(subscriber => subscriber(...data))
+    }
+  }

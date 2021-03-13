41bc8008be1710908dd2c7e20ee1d9ac690b777d
gulevskydev <d.gulevsky@mail.ru>
2021-03-13 10:15:57 +0300

    6kyu

---
+++ 6kyu/Simple Fun #15: Addition without Carrying.js
@@ -0,0 +1,24 @@
+function additionWithoutCarrying(a, b) {
+  a = String(a);
+  b = String(b);
+  let aLen = a.length - 1;
+  let bLen = b.length - 1;
+  let ans = [];
+  while (aLen >= 0 || bLen >= 0) {
+    if (aLen < 0) {
+      ans.unshift(b[bLen]);
+      bLen--;
+    }
+    if (0 > bLen) {
+      ans.unshift(a[aLen]);
+      aLen--;
+    } else {
+      const cur = String(+a[aLen] + +b[bLen]);
+      console.log(+String(cur)[cur.length - 1]);
+      ans.unshift(+String(cur)[cur.length - 1]);
+      aLen--;
+      bLen--;
+    }
+  }
+  return +ans.join('');
+}

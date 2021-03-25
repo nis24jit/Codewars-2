0639372620320b6f3b3fd25a17e17c66a19cf708
gulevskydev <d.gulevsky@mail.ru>
2021-03-25 22:36:58 +0300

    7kyu

--- 7kyu/Simple letter removal.js
+++ 7kyu/Simple letter removal.js
@@ -1,14 +1,13 @@
-//Simple letter removal
 function solve(s, k) {
-    const sk = s.split('').sort().slice(0, k)
-    const obj = {};
-    sk.forEach(v => obj[v] = obj[v] ? obj[v] + 1 : 1)
-    s = s.split('').map(e => {
-        if (obj[e] > 0) {
-            obj[e]--
-            return ''
-        }
-        return e
-    })
-    return s.join('')
+  const sk = s.split('').sort().slice(0, k);
+  const obj = {};
+  sk.forEach((v) => (obj[v] = obj[v] ? obj[v] + 1 : 1));
+  s = s.split('').map((e) => {
+    if (obj[e] > 0) {
+      obj[e]--;
+      return '';
+    }
+    return e;
+  });
+  return s.join('');
 }

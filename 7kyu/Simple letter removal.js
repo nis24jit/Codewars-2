166b3ad4efeef6a67732bc16fd3beb822b132d10
gulevskydev <d.gulevsky@mail.ru>
2021-03-26 21:26:32 +0300

    7kyu

--- 7kyu/Simple letter removal.js
+++ 7kyu/Simple letter removal.js
@@ -1,6 +1,7 @@
 function solve(s, k) {
   const sk = s.split('').sort().slice(0, k);
   const obj = {};
+
   sk.forEach((v) => (obj[v] = obj[v] ? obj[v] + 1 : 1));
   s = s.split('').map((e) => {
     if (obj[e] > 0) {

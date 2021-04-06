caa8966067b448a5eed091436eb5d6c9ddf92c6e
gulevskydev <d.gulevsky@mail.ru>
2021-04-06 17:41:29 +0300

    7kyu

--- 7kyu/Life Path Number.js
+++ 7kyu/Life Path Number.js
@@ -1,9 +1,13 @@
-//Life Path Number
-
 function lifePathNumber(dateOfBirth) {
-    return getNumber(dateOfBirth.split('-').reduce((a,b)=>a+getNumber(b),0));
-  }
-  function getNumber(a){
-    if(a<10) return +a
-    return getNumber(String(a).split('').reduce((a,b) => +a + +b))
-  }
+  return getNumber(
+    dateOfBirth.split('-').reduce((a, b) => a + getNumber(b), 0)
+  );
+}
+function getNumber(a) {
+  if (a < 10) return +a;
+  return getNumber(
+    String(a)
+      .split('')
+      .reduce((a, b) => +a + +b)
+  );
+}

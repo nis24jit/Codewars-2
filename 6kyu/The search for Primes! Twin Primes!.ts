a0ac5e8d05a753497dbe364e9a3c20da6230332b
Dmitiy Gulevskiy <gulevskydev@yandex-team.ru>
2021-08-15 14:30:10 +0300

    6 kyu

---
+++ 6kyu/The search for Primes! Twin Primes!.ts
@@ -0,0 +1,15 @@
+const findPrime = (n:number): boolean => {
+    for(let i = 2; i < n; i++) {
+      if(n % i === 0) return false
+    } 
+    return n > 1
+  }
+  
+  export const twinPrime = (n: number): number => {
+    const twinPrimes: number[] = []
+    for(let i = 2; i <= n+1; i++) {
+      const isPrime = findPrime(i) 
+      isPrime && twinPrimes.push(i)
+    }
+    return twinPrimes.filter((num,i) => num + 2 === twinPrimes[i+1]).length
+  }
